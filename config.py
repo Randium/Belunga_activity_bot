8b86942c3e65a1fa69c933596e59463a547a2dc9
Randium <33968886+Randium@users.noreply.github.com>
2018-06-27 12:14:27 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+'''This is the channel where the bot tells it has gone online, and it uses the channel to remember what its main server is.'''
+welcome_channel = 457270968440848385
+
+'''This is the current database file. When changing this name,
+make sure to format with reset.py if you are unsure if the file has been formatted yet.'''
+database = 'database.db'
+
+'''The prefix that the bot should accept for its commands'''
+prefix = '!'
+
+'''The token to log in with your bot. This is like a password, do not share this with anyone!'''
+TOKEN = 'your_token_here_please'
